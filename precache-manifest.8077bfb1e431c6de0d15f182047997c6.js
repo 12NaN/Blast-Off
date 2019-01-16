a0ce67d7ccc2c0d459ff5ef2fc56574adfa97ed2
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-01-16 15:36:08 -0500

    Updates

--- precache-manifest.8077bfb1e431c6de0d15f182047997c6.js
+++ precache-manifest.8077bfb1e431c6de0d15f182047997c6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Blast-Off/static/js/runtime~main.74b46de1.js"
   },
   {
-    "revision": "2e3b3405e071795beb0d",
-    "url": "/Blast-Off/static/js/main.2e3b3405.chunk.js"
+    "revision": "1763401542561fef4900",
+    "url": "/Blast-Off/static/js/main.17634015.chunk.js"
   },
   {
     "revision": "bb4f17aac363556c6467",
     "url": "/Blast-Off/static/js/1.bb4f17aa.chunk.js"
   },
   {
-    "revision": "2e3b3405e071795beb0d",
+    "revision": "1763401542561fef4900",
     "url": "/Blast-Off/static/css/main.d475256c.chunk.css"
   },
   {
-    "revision": "92bf132ca893c39659bfe67df0957af6",
+    "revision": "ddec29ff8981a3d44e11dc26eb72e1a5",
     "url": "/Blast-Off/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Blast-Off/precache-manifest.69be1445f8d46fe56360603df49c8640.js"
+  "/Blast-Off/precache-manifest.8077bfb1e431c6de0d15f182047997c6.js"
 );
 
 workbox.clientsClaim();
