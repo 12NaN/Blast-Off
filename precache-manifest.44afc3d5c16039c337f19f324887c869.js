7ed9d742157e791ddb3882b25ee2533d96735603
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-01-16 13:53:24 -0500

    Updates

---
+++ precache-manifest.44afc3d5c16039c337f19f324887c869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74b46de168f84590d260",
+    "url": "/Blast-Off/static/js/runtime~main.74b46de1.js"
+  },
+  {
+    "revision": "1ba1fb473b21ed93f9dc",
+    "url": "/Blast-Off/static/js/main.1ba1fb47.chunk.js"
+  },
+  {
+    "revision": "50e6027d6f0d9938d79c",
+    "url": "/Blast-Off/static/js/1.50e6027d.chunk.js"
+  },
+  {
+    "revision": "1ba1fb473b21ed93f9dc",
+    "url": "/Blast-Off/static/css/main.ea4ec366.chunk.css"
+  },
+  {
+    "revision": "e6778d239da8c19e42f599207e0a9ade",
+    "url": "/Blast-Off/index.html"
+  }
+];
