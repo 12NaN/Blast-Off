c6288073c6177311f05135d424d71b10f63e6b84
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-01-16 14:22:44 -0500

    Updates

---
+++ precache-manifest.69be1445f8d46fe56360603df49c8640.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74b46de168f84590d260",
+    "url": "/Blast-Off/static/js/runtime~main.74b46de1.js"
+  },
+  {
+    "revision": "2e3b3405e071795beb0d",
+    "url": "/Blast-Off/static/js/main.2e3b3405.chunk.js"
+  },
+  {
+    "revision": "bb4f17aac363556c6467",
+    "url": "/Blast-Off/static/js/1.bb4f17aa.chunk.js"
+  },
+  {
+    "revision": "2e3b3405e071795beb0d",
+    "url": "/Blast-Off/static/css/main.d475256c.chunk.css"
+  },
+  {
+    "revision": "92bf132ca893c39659bfe67df0957af6",
+    "url": "/Blast-Off/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Blast-Off/precache-manifest.44afc3d5c16039c337f19f324887c869.js"
+  "/Blast-Off/precache-manifest.69be1445f8d46fe56360603df49c8640.js"
 );
 
 workbox.clientsClaim();
